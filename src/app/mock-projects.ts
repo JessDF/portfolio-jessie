d64aa2014b5343d9d0e099e017cefab4fc9d1225
Jessie Dowding <jdowding@csumb.edu>
2018-03-07 04:01:32 +0000

    Initial project

---
+++ src/app/mock-projects.ts
@@ -0,0 +1,13 @@
+import { Project } from './project';
+
+export const PROJECTS: Project[] = [
+  { id: 1, name: 'MovieProject', description: "project" },
+  { id: 2, name: 'Portfolio', description: "project"  },
+  { id: 3, name: 'multiuserblog', description: "project"  },
+  { id: 4, name: 'Tournament', description: "project"  },
+  { id: 5, name: 'LogsAnalysisProject', description: "project"  },
+  { id: 6, name: 'itemCatalog', description: "project"  },
+  { id: 7, name: 'Neighborhood Map', description: "project"  },
+  { id: 8, name: 'Linux Server', description: "project"  }
+];
+
